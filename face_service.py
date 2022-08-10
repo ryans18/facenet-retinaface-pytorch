5f96f69b8e074b0529fd0efbdc0f66ae32837b33
RyansRen <ryansren@bestaxc.com.cn>
2022-08-10 18:36:16 +0800

    update

--- face_service.py
+++ face_service.py
@@ -2,6 +2,7 @@ import json
 import cv2
 import numpy as np
 import traceback
+import base64
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 from retinaface_api import RetinafaceApi
@@ -15,12 +16,14 @@ def index():
     }
     return json.dumps(result)
 
-@app.route('/regist', methods=['POST'])
+@app.route('/regist', methods=['GET'])
 def regist():
     try:
+        base64Data = request.args.get('img')
+        img = base64.b64decode(base64Data)
          # file就是接收file对象
-        file = request.files.get('file')
-        retinaface.save_face(file)
+        # file = request.files.get('file')
+        retinaface.save_face(img)
         retinaface.encode_face_dataset()
         retinaface.load_face()
         return jsonify({'status': 200})
@@ -28,7 +31,20 @@ def regist():
         print(traceback.print_exc())
         return jsonify({'status': 500})
 
-@app.route('/face', methods=['POST'])
+@app.route('/face', methods=['GET'])
+def face():
+    try:
+        base64Data = request.args.get('img')
+        img = base64.b64decode(base64Data)
+        image = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
+        image   = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        valid = retinaface.detect_image_2_name(image)
+        return jsonify({'status': 200, 'valid': valid})
+    except Exception as e:
+        print(traceback.print_exc())
+        return jsonify({'status': 500})
+
+''' @app.route('/face', methods=['POST'])
 def face():
     try:
          # file就是接收file对象
@@ -40,7 +56,7 @@ def face():
         return jsonify({'status': 200, 'valid': valid})
     except Exception as e:
         print(traceback.print_exc())
-        return jsonify({'status': 500})
+        return jsonify({'status': 500}) '''
 
 if __name__ == '__main__':
     retinaface = RetinafaceApi()
