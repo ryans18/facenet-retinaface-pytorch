f0a3412953a47b723aab6cbe33f1cb4f6c6c2d16
RyansRen <ryansren@bestaxc.com.cn>
2022-08-11 18:43:37 +0800

    update

--- face_service.py
+++ face_service.py
@@ -11,25 +11,30 @@ from retinaface_api import RetinafaceApi
 @app.route("/")
 def index():
     result = {
-        'satus' : '200',
+        'code' : '200',
         'data': 'Hello, world'
     }
     return json.dumps(result)
 
-@app.route('/regist', methods=['GET'])
+@app.route('/regist', methods=['POST'])
 def regist():
     try:
-        base64Data = request.args.get('img')
+        form = request.form
+        base64Data = form.get('img')
+        # base64Data = request.args.get('img')
         img = base64.b64decode(base64Data)
          # file就是接收file对象
         # file = request.files.get('file')
         retinaface.save_face(img)
-        retinaface.encode_face_dataset()
-        retinaface.load_face()
-        return jsonify({'status': 200})
+        face = retinaface.encode_face_dataset()
+        if face != 0:
+            retinaface.load_face()
+            return jsonify({'code': 200, 'success': True, 'face': face})
+        else:    
+            return jsonify({'code': 200, 'success': False, 'face': face})
     except Exception:
         print(traceback.print_exc())
-        return jsonify({'status': 500})
+        return jsonify({'code': 500})
 
 @app.route('/face', methods=['POST'])
 def face():
@@ -42,11 +47,11 @@ def face():
         img = base64.b64decode(base64Data)
         image = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
         image   = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        valid = retinaface.detect_image_2_name(image)
-        return jsonify({'status': 200, 'valid': valid})
+        face, valid = retinaface.detect_image_2_name(image)
+        return jsonify({'code': 200, 'success': valid, 'face': face})
     except Exception as e:
         print(traceback.print_exc())
-        return jsonify({'status': 500})
+        return jsonify({'code': 500})
 
 ''' @app.route('/face', methods=['POST'])
 def face():
